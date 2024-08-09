7f4fc6242f9252716acd527aaabec7507c26cbae
Henry <studyliuhuan@163.com>
2024-08-09 10:48:58 +0800

    UI调整: 播放器悬浮于整体页面之上

--- src/components/media/MusicPlay.vue
+++ src/components/media/MusicPlay.vue
@@ -113,6 +113,9 @@
     background-color: rgba(255, 255, 255, 0.8);
     border-radius: 8px;
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    position: absolute;
+    bottom: 5px;
+    width: 98%;
   }
 
   .thumbnail {
